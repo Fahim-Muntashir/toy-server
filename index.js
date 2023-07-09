cb77529ad34fae2943ae51dc2cefac98a357ebeb
Md Fahim Muntashir <101015239+FahimMuntashir1@users.noreply.github.com>
2023-07-09 22:09:50 +0600

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("MongoDB");
 
 app.use(cors({
-    origin: 'http://example.com', // use your actual domain name (or localhost), using * is not recommended
+    origin: 'https://auth-firebase-context-ta-e747a.web.app/', 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
     credentials: true
